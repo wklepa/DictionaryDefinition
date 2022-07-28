3baeab3fc2d43cc6db56dbe7e00ff4e9043a6f11
Wojciech Klepacki <wklepa@outlook.com>
2022-07-28 15:14:41 +1000

    Working

--- DictionaryDefinition/Program.cs
+++ DictionaryDefinition/Program.cs
@@ -10,8 +10,13 @@ var cities = new Dictionary<string, string>()
 // Extract and combine countries into a single string
 string combinedCities = "";
 var keys = new List<string>();
-foreach(var kvp in cities)
+var lowercountry = new List<string>();
+foreach (var kvp in cities)
+{
     keys.Add(kvp.Key);
+    lowercountry.Add((kvp.Key).ToLower());
+}
+
 combinedCities = String.Join(',', keys);
 
 // Check if given country is in the dictionary
@@ -19,28 +24,28 @@ while (true)
 {
     // Ask for country name
     string? userCountry = "";
+    string userUpper = "";
     Console.Write("Pick one of the following countries: {0}: ", combinedCities);
-    // First trim the white spaces from the string
-    userCountry = Console.ReadLine().Trim(' ');
+
+    // Trim the white spaces from the string and convert in to lowercase
+    userCountry = Console.ReadLine().Trim(' ').ToLower();
     /* Now uppercase the first letter and concatenate it with the rest of the string
        or uppercase the whole string if the length is less than three*/
-    if (userCountry.Length >= 3)
-        userCountry = char.ToUpper(userCountry[0]) + userCountry.Substring(1);
+    if (userCountry.Length > 3)
+        userUpper = char.ToUpper(userCountry[0]) + userCountry.Substring(1);
     else
-        userCountry = userCountry.ToUpper();
+        userUpper = userCountry.ToUpper();
 
     // Check if the country is not empty and is in the defined dictionary
     if (userCountry != "")
     {
-
-        if (cities.ContainsKey(userCountry))
+        if (lowercountry.Contains(userCountry))
         {
-            Console.WriteLine("{0} has following significant cities: {1}", userCountry, cities[userCountry]);
+            Console.WriteLine("{0} has following significant cities: {1}", userUpper, cities[userUpper]);
             break;
         }
         else
-            Console.WriteLine("{0} doesn't have any major city. Try again.\n", userCountry);
-
+            Console.WriteLine("{0} doesn't have any major city. Try again.\n", userUpper);
     }
     else
         Console.WriteLine("No user input, try again.\n");
